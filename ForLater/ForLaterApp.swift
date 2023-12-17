22fcbe42ce79369db0d6e1d2e4f5465f0a72629c
Gregory Sin-Chan <gregory@sin-chan.com>
2023-12-16 21:08:00 -0500

    Initial Commit

---
+++ ForLater/ForLaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForLaterApp.swift
+//  ForLater
+//
+//  Created by Gregory Sin-Chan on 2023-12-16.
+//
+
+import SwiftUI
+
+@main
+struct ForLaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
